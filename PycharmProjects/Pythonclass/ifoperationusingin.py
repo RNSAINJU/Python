40f88818df8916fe124eafb257734e92f2e5d1af
aryan.sainju@gmail.com <aryan.sainju@gmail.com>
2018-09-18 21:49:28 +0545

    Using range in

---
+++ PycharmProjects/Pythonclass/ifoperationusingin.py
@@ -0,0 +1,17 @@
+a=(int)(input("Enter your favourite number:"))
+print(a)
+if (a in range (0,4)):
+    if a==3:
+        print("Your number is 3")
+    elif a==2:
+        print("Your number is 2")
+    elif a==1:
+        print("Your number is 1")
+    else:
+        print("YOur number is 0")
+elif (a in range (4,8)):
+    print("Your number is less than or equal to 7")
+elif (a in range (8,10)):
+    print("Your number is less than or equal to 9")
+else:
+    print("Unexpected input")
