9a0db2e4b84b5cc8563e3d70fdef957de18b6d2e
aryan.sainju@gmail.com <aryan.sainju@gmail.com>
2018-09-19 08:31:53 +0545

    for

---
+++ PycharmProjects/Pythonclass/for.py
@@ -0,0 +1,26 @@
+# for i in range(5,15):
+#     type(print(i))
+
+#a= start, b=stop, c =interval
+# for i in range(5,51,5):
+#     print(i)
+
+# for i in range(17,71,17):
+#     print(i)
+
+
+# for i in 'aryan':
+#     print(i)
+
+# for i in 5,6:
+#     print(i)
+
+# for i in True, False,5,45.0,'name':
+#     print(i)
+
+for i in range(21):
+    if i%2 ==0:
+        pass
+    else:
+        print(i)
+
