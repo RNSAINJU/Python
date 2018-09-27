ab96462144740a05c1f4475e9f36c110d4397777
RNSAINJU <Aryan.sainju@gmail.com>
2018-09-27 20:59:06 +0545

    Add files via upload

---
+++ list.py
@@ -0,0 +1,44 @@
+#Create a List:
+# thislist = ["apple","banana","cherry"]
+
+#Print the second item of the list:
+# print(thislist[1])
+
+#Change the second item:
+# thislist[1]="blackcurrent"
+
+#Print all items in the list, one by one:
+# for x in thislist:
+#      print(x)
+
+# Print the number of items in the list:
+# print(len(thislist))
+
+# Using the append() method to add an item:
+# thislist.append("orange")
+# print(thislist)
+
+# Insert an item as the second position:
+# thislist.insert(1,"mango")
+
+# The remove() method removes the specified item:
+# thislist.remove("banana")
+
+# The pop() method removes the specified index, (or the last item if index is not specified):
+# thislist.pop()
+
+# The del keyword removes the specified index:
+# del thislist[0]
+
+# The del keyword can also delete the list completely:
+# del thislist
+
+# The clear() method empties the list:
+# thislist.clear()
+
+# Using the list() constructor to make a List:
+# thislist1=list((thislist))
+
+
+
+
